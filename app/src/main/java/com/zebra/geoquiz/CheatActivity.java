f9f0904afc127c0b40c201bfea2f7d5d2f0c28f4
HLi <harveyli508@hotmail.com>
2018-10-24 15:41:30 +0800

    Add version compatibility

--- app/src/main/java/com/zebra/geoquiz/CheatActivity.java
+++ app/src/main/java/com/zebra/geoquiz/CheatActivity.java
@@ -1,9 +1,13 @@
 package com.zebra.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -37,6 +41,24 @@ public class CheatActivity extends AppCompatActivity {
                 Intent data = new Intent();
                 data.putExtra(EXTRA_ANSWER_SHOWN, true);
                 setResult(RESULT_OK, data);
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswerButton.getWidth() / 2;
+                    int cy = mShowAnswerButton.getHeight() / 2;
+                    float radius = mShowAnswerButton.getWidth();
+                    Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswerButton,
+                            cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswerButton.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mShowAnswerButton.setVisibility(View.INVISIBLE);
+                }
             }
         });
     }
